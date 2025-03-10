a9be8fc7d92279732f865669bd9414c96a36a311
Jesse Kela <jessekkela@gmail.com>
2025-03-10 08:52:30 +0200

    Loading component

---
+++ client/src/components/Loading.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="spinner-container" aria-label="loading">
+    <font-awesome-icon :icon="faSpinner" :spin="true" :size="size" />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue"
+  import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+  import { faSpinner } from "@fortawesome/free-solid-svg-icons"
+
+  export default defineComponent({
+    name: "Loading",
+    components: {
+      FontAwesomeIcon,
+    },
+    props: {
+      size: {
+        type: String,
+        default: "1x",
+      },
+    },
+    setup() {
+      return {
+        faSpinner,
+      }
+    },
+  })
+</script>
+
+<style scoped>
+  .spinner-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px;
+  }
+</style>
