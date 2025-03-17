9e4f18041e91c15236dc84137a8ba58458a0126c
Jesse Kela <jessekkela@gmail.com>
2025-03-17 22:36:10 +0200

    CSP to allow swagger UI

--- server/src/middleware/secure_headers.py
+++ server/src/middleware/secure_headers.py
@@ -4,7 +4,15 @@ from starlette.middleware.base import BaseHTTPMiddleware
 class SecureHeadersMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
-        response.headers["Content-Security-Policy"] = "default-src 'self'; script-src 'self'"
+        response.headers["Content-Security-Policy"] = (
+            "default-src 'self'; "
+            "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.cloudflare.com https://unpkg.com https://cdn.jsdelivr.net; "
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; "
+            "font-src 'self' https://fonts.gstatic.com; "
+            "img-src 'self' https://fastapi.tiangolo.com; "
+            "connect-src 'self'; "
+            "frame-src 'self' https://cdn.jsdelivr.net;"
+        )
         response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
         response.headers["X-Content-Type-Options"] = "nosniff"
         response.headers["X-Frame-Options"] = "SAMEORIGIN"
