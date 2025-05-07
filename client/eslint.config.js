500ccf26bd68c5a3d2e29f41abed7513ce37cb6c
Jesse Kela <jessekkela@gmail.com>
2025-05-07 23:14:42 +0300

    Add eslint sonar plugin

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -7,6 +7,7 @@ import vuePlugin from "eslint-plugin-vue"
 import importPlugin from "eslint-plugin-import"
 import securityPlugin from "eslint-plugin-security"
 import playwrightPlugin from "eslint-plugin-playwright"
+import sonarPlugin from "eslint-plugin-sonarjs"
 
 export default [
   js.configs.recommended,
@@ -37,17 +38,20 @@ export default [
       vue: vuePlugin,
       import: importPlugin,
       security: securityPlugin,
+      sonarjs: sonarPlugin,
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
       ...importPlugin.configs.recommended.rules,
       ...securityPlugin.configs.recommended.rules,
+      ...sonarPlugin.configs.recommended.rules,
       "@typescript-eslint/consistent-type-imports": [
         "error",
         { prefer: "type-imports" },
       ],
       "import/no-unresolved": "off",
       "security/detect-object-injection": "off",
+      "sonarjs/todo-tag": "off",
       semi: ["error", "never"],
       "no-undef": "warn",
       "no-unused-vars": "warn",
