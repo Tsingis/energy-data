787526a1d394b97235c9d9c94b0c766be101f96c
Jesse Kela <jessekkela@gmail.com>
2025-05-21 22:35:14 +0300

    Fix formatting

--- client/src/public/insert-links.js
+++ client/src/public/insert-links.js
@@ -2,25 +2,25 @@
   try {
     const apiBaseUrl = new URL("__API_URL__")
     const head = document.head
-  
+
     const preconnectLink = document.createElement("link")
     preconnectLink.rel = "preconnect"
     preconnectLink.href = apiBaseUrl.origin
     preconnectLink.crossOrigin = "anonymous"
     head.appendChild(preconnectLink)
-  
+
     const dnsPrefetchLink = document.createElement("link")
     dnsPrefetchLink.rel = "dns-prefetch"
     dnsPrefetchLink.href = apiBaseUrl.origin
     head.appendChild(dnsPrefetchLink)
-  
+
     const preloadLinkEnergy = document.createElement("link")
     preloadLinkEnergy.rel = "preload"
     preloadLinkEnergy.href = `${apiBaseUrl}api/energy`
     preloadLinkEnergy.as = "fetch"
     preloadLinkEnergy.crossOrigin = "anonymous"
     head.appendChild(preloadLinkEnergy)
-  
+
     const preloadLinkPrice = document.createElement("link")
     preloadLinkPrice.rel = "preload"
     preloadLinkPrice.href = `${apiBaseUrl}api/price`
