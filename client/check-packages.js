c139cb98195566963d6ef1eb7b27f377d013ede2
Jesse Kela <jessekkela@gmail.com>
2025-08-06 20:26:37 +0300

    Node protcol for imports

--- client/check-packages.js
+++ client/check-packages.js
@@ -1,6 +1,6 @@
-import fs from "fs"
-import path from "path"
-import { execSync } from "child_process"
+import fs from "node:fs"
+import path from "node:path"
+import { execSync } from "node:child_process"
 
 const packageLockPath = path.resolve("package-lock.json")
 const nodeModulesPath = path.resolve("node_modules")
