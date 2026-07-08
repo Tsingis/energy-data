b024782b0283192f6ac1a2493c46043703b99b7e
Jesse Kela <jessekkela@gmail.com>
2026-07-08 23:43:27 +0300

    Fix vite rollup options input path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     outDir: "../dist",
     emptyOutDir: true,
     rollupOptions: {
-      input: "src/index.html",
+      input: "index.html",
     },
   },
   server: {
